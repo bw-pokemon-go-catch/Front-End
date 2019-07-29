c8460ce8f78819af4de71b6b142d74dd27dab9bb
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-29 16:27:17 -0400

    performed an axios get request and successfully pulled data from the back end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,17 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+
+  useEffect( () => {
+    axios.get('https://pokemon-go-bw-backend.herokuapp.com/pokemon')
+      .then( response => {
+        console.log(response)
+      })
+      .catch( error => {
+        console.log(error)
+      });
+  }, []);
+
   return (
     <div className="App">
       hi
