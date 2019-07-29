ba7a96e39c6e08a7b55070ded0ee89eb1fc7c02a
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-29 17:56:06 -0400

    installed react router and created `AppRouter` component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,24 +7,33 @@ import SignupPage from './components/signup';
 
 function App() {
 
-  const [pokemonData, setPokemonData] = useState();
+  //hooks
+  const [pokemonName, setPokemonName] = useState();
 
   useEffect( () => {
     axios.get('https://pokemon-go-bw-backend.herokuapp.com/pokemon')
       .then( response => {
-        console.log(response.data[0])
-        console.log(response.data[0]['MAX HP'])
-        setPokemonData(response.data[0]['MAX CP'])
+        console.log(response)
+        console.log(response.data[0]['Max HP'])
+        setPokemonName(response.data[0]['Name'])
       })
       .catch( error => {
         console.log(error)
       });
   }, []);
 
+  //event handlers
+  const handleCalculation = event => {
+
+  }
+
   return (
     <div className="App">
-      Gotta catch 'em all!
-      {pokemonData}
+      Should I try to catch this Pokemon?
+      <br />
+      <form onSubmit={event => handleCalculation(event)}>
+      </form>
+      <br />
       <SignupPage />
       <LoginPage />
     </div>
