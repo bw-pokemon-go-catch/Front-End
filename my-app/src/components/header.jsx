0e0944edee9a03aa3fae24a1ca5d552ccc9d8190
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-01 11:41:23 -0400

    resting netlify continuous deployment

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Worthymon</h1>
+      <h1>test</h1>
       <h4 className="statement">Should you catch this Pokemon?</h4>
     </header>
   );
